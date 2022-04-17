546afcc4971fbfa2b78891dd4e555a10ebd6dfad
Manas Paul <manas@codelogicx.com>
2022-04-17 15:05:16 +0530

    :feat: User model created

---
+++ models/user.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID        uuid.UUID `gorm:"type:uuid;primary_key"`
+	Firtsname string    `json:"first_name" gorm:"type:varchar(255)"`
+	Lastname  string    `json:"last_name" gorm:"type:varchar(255)"`
+	Email     string    `json:"email" validate:"required,email,min=6,max=32" gorm:"type:varchar(255);not null;unique"`
+	Password  string    `json:"password" gorm:"type:varchar"`
+
+	Varifired bool `json:"is_varified" gorm:"type:boolean default:false"`
+
+	// TODO needs to covert to ENUM type later
+	UserType     string `json:"user_type" gorm:"type:varchar(100) default:'user'"`
+	Authprovider string `json:"auth_provider" gorm:"type:varchar(60) default:'google'"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	// UUID version 4
+	user.ID = uuid.New()
+	return
+}
